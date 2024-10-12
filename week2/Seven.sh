a9569bb5e7936762dab515a8741b48619278d926
Rajat Bhattacharjya <rajatbh21@gmail.com>
2024-10-11 22:18:50 -0700

    add correction

--- week2/Seven.sh
+++ week2/Seven.sh
@@ -2,5 +2,7 @@
 
 stops=$(tr ',' '\n' < ../stop_words.txt; echo {a..z})
 words=$(tr -sc 'A-Za-z' '\n' < "$1" | tr A-Z a-z | grep -vFx -f <(echo "$stops"))
-echo "$words" | sort | uniq -c | sort -nr | head -n 25
+sorted_words=$(echo "$words" | sort | uniq -c | sort -nr | head -n 25)
+
+echo "$sorted_words" | awk '{print $2 " - " $1}'
 
