08c1082b1a3c1f560791c00ed7c83c1a081a60b2
Rajat Bhattacharjya <rajatbh21@gmail.com>
2024-10-11 22:32:04 -0700

    correct- final I think

--- week2/Seven.sh
+++ week2/Seven.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-stops=$(tr ',' '\n' < ../stop_words.txt; echo {a..z})
-words=$(tr -sc 'A-Za-z' '\n' < "$1" | tr A-Z a-z | grep -vFx -f <(echo "$stops"))
-sorted_words=$(echo "$words" | sort | uniq -c | sort -nr | head -n 25)
-
-echo "$sorted_words" | awk '{print $2 " - " $1}'
-
+stopwords=$(tr ',' '\n' < ../stop_words.txt)
+words=$(grep -oE '[a-zA-Z]{2,}' "$1" | tr 'A-Z' 'a-z' | grep -vFx -f <(echo "$stopwords"))
+echo "$words" | sort | uniq -c | sort -nr | head -n 25 | awk '{print $2 " - " $1}'
