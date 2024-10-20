e5c7e6dc1072de13be518b5b093c02dbce884346
Rajat Bhattacharjya <rajatbh21@gmail.com>
2024-10-19 20:06:40 -0700

    modified eight

--- week3/Eight.cs
+++ week3/Eight.cs
@@ -15,6 +15,7 @@ namespace EightProg
 
         public static string FilterCharsAndNormalize(string data)
         {
+            
             return Regex.Replace(data, "[^a-zA-Z]", " ").ToLower();
         }
 
@@ -26,7 +27,7 @@ namespace EightProg
         public static List<string> RemoveStopWords(List<string> words)
         {
             var stop_words = File.ReadAllText("../stop_words.txt").Split(",").ToList();
-            
+           
             var temp = new List<string>(words);
             
             temp.RemoveAll(s => (stop_words.Contains(s) || s.Length < 2));
@@ -34,36 +35,60 @@ namespace EightProg
             return temp;
         }
 
-        // Recursive frequency counting function
+      
         public static void CountFrequencies(List<string> words, Dictionary<string, int> frequency)
         {
-            // Base case
-            if (words.Count == 0)
-                return;
-
-            
-            string word = words[0];
-            if (frequency.ContainsKey(word))
+            foreach (string word in words)
             {
-                frequency[word]++;
+                if (frequency.ContainsKey(word))
+                {
+                    frequency[word]++;
+                }
+                else
+                {
+                    frequency.Add(word, 1);
+                }
             }
-            else
-            {
-                frequency.Add(word, 1);
-            }
-
-            
-            CountFrequencies(words.Skip(1).ToList(), frequency);
         }
 
-        
-        public static Dictionary<string, int> Sort(Dictionary<string, int> frequency)
+        // Recursive QuickSort 
+        public static List<KeyValuePair<string, int>> RecursiveSort(List<KeyValuePair<string, int>> frequencyList)
         {
             
-            return frequency.OrderByDescending(a => a.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            if (frequencyList.Count <= 1)
+            {
+                return frequencyList;
+            }
+
+           //pivot is first element here
+            var pivot = frequencyList[0];
+
+            // let's do 2 parts: '<' and '>' pivot
+            var lesser = new List<KeyValuePair<string, int>>();
+            var greater = new List<KeyValuePair<string, int>>();
+
+            for (int i = 1; i < frequencyList.Count; i++)
+            {
+                if (frequencyList[i].Value >= pivot.Value)
+                {
+                    greater.Add(frequencyList[i]);
+                }
+                else
+                {
+                    lesser.Add(frequencyList[i]);
+                }
+            }
+
+            // Recursive sort
+            var sorted = new List<KeyValuePair<string, int>>();
+            sorted.AddRange(RecursiveSort(greater)); 
+            sorted.Add(pivot);
+            sorted.AddRange(RecursiveSort(lesser));
+
+            return sorted;
         }
 
-        
+       
         public static void PrintFrequencies(List<KeyValuePair<string, int>> sortedFrequencies, int index)
         {
             
@@ -74,27 +99,27 @@ namespace EightProg
             var kvp = sortedFrequencies[index];
             Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
 
-            
+           
             PrintFrequencies(sortedFrequencies, index + 1);
         }
 
         static int Main(string[] args)
         {
-            
+           
             if (args.Length == 0)
             {
                 Console.WriteLine("Please enter input text file");
                 return 1;
             }
 
-            
+           
             if (!File.Exists(args[0]))
             {
                 Console.WriteLine("Could not find file {0}", args[0]);
                 return 1;
             }
 
-            
+           
             string data = ReadFile(args[0]);
             string normalizedData = FilterCharsAndNormalize(data);
             List<string> words = Scan(normalizedData);
@@ -105,9 +130,12 @@ namespace EightProg
             CountFrequencies(words, wordFreqs);
 
             
-            var sortedFreqs = Sort(wordFreqs).ToList();
+            var freqList = wordFreqs.ToList();
 
             
+            var sortedFreqs = RecursiveSort(freqList);
+
+           
             PrintFrequencies(sortedFreqs, 0);
 
             return 0;
